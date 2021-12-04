9ad773c57aba766eab8cad752f5993e37d6c664a
Igorwir <95366172+Igorwir@users.noreply.github.com>
2021-12-04 22:19:25 +0300

    Add files via upload

---
+++ src/Hello_World.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Hello_World {
+    public static  void main(String[] args) {
+        System.out.println("Hello team !");
+        Scanner sc = new Scanner(System.in);
+        String samName = sc.nextLine();
+    }
+}
